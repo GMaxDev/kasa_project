26111601f4c0b95e6dfc46e2114d6d9ab07c8071
GMaxDev <maxime.guigra@gmail.com>
2022-11-15 21:16:27 +0100

    ajout de la data des logements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import styles from '../styles/Card.module.scss'
 
 function Card() {
     return(
-        <div className={`${styles.card} b2`}>
+        <div className={`${styles.card}`}>
         </div>
     )
 }
