ee7a7bf2d811a531bb4dc8022affa4408f90a259
GMaxDev <maxime.guigra@gmail.com>
2022-11-24 23:01:44 +0100

    About page OK

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,10 +7,18 @@ function About() {
     return (
         <main className={styles.app}>
             <Banner imgSrc={kasaBannerAbout} alt="Paysage montagneux avec des nuages"/>
-            <InfoBloc title="Titre 1" content="Texte random 1"/>
-            <InfoBloc title="Titre 2" content="Texte random 2"/>
-            <InfoBloc title="Titre 3" content="Texte random 3"/>
-            <h2>About</h2>
+            <InfoBloc title="Fiabilité" content="
+                Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.
+            "/>
+            <InfoBloc title="Respect" content="
+                La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
+            "/>
+            <InfoBloc title="Service" content="
+                Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
+            "/>
+            <InfoBloc title="Sécurité" content="
+                La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
+            "/>
         </main>
     )
 }
