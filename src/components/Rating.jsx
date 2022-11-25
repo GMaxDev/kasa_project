7a8ed5c1c7c5af0debb6836ba46727b253f6d3ce
GMaxDev <maxime.guigra@gmail.com>
2022-11-26 00:46:15 +0100

    système de rating ok

---
+++ src/components/Rating.jsx
@@ -0,0 +1,30 @@
+import '../styles/Rating.module.scss'
+import greyRating from '../assets/img/star_rate.svg'
+import redRating from '../assets/img/star_rate_red.svg'
+
+function Rating(rating)  {
+    const stars = [1, 2, 3, 4, 5]
+
+    return (
+        <div className="rating">
+            {stars.map((star) => rating >= star ? (
+                <img
+                    key={star.toString()}
+                    className="rating__icon"
+                    src={redRating}
+                    alt=""
+                />
+            ) : (
+                <img
+                    key={star.toString()}
+                    className="rating__icon"
+                    src={greyRating}
+                    alt=""
+                />
+                )
+            )}
+        </div>
+    )
+}
+
+export default Rating
--- src/pages/logement/logement.jsx
+++ src/pages/logement/logement.jsx
@@ -1,16 +1,20 @@
 import ApartmentList from '../../data/apartment.json'
+import Rating from '../../components/Rating'
+
 import { useParams } from "react-router-dom"
+
 
 const Logement = () => {
     const { params } = useParams()
     const product = ApartmentList.find((a) => a.id === params)
 
-    const { title, location, rating, host, equipments, description, pictures } = product
+    // const { title, location, rating, host, equipments, description, pictures } = product
 
     console.log(product)
 
     return (
         <main>
+            <Rating rating='3'/>
             <h2>Logement</h2>
         </main>
     )
