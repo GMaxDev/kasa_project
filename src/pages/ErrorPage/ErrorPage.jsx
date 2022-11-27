ca565fb9765fb7c35153f2eb63eb89c84142bde6
GMaxDev <maxime.guigra@gmail.com>
2022-11-27 17:27:39 +0100

    correction du header sur le pagenotfound

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,11 +3,9 @@ import Header from "../../components/Header"
 import { NavLink, useRouteError } from "react-router-dom"
 
 const ErrorPage = () => {
-    const error = useRouteError()
 
     return (
         <div className={styles.errorPage}>
-            <Header />
             <div className={styles.titles}>
                 <h1>404</h1>
                 <h2>Oups! La page que vous demandez n'existe pas.</h2>
