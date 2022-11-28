c451b8d2c3ddfccfc5abfeb9944d174bdb18dfaf
GMaxDev <maxime.guigra@gmail.com>
2022-11-28 23:59:03 +0100

    finalisation page logement

--- src/pages/logement/logement.jsx
+++ src/pages/logement/logement.jsx
@@ -4,42 +4,43 @@ import ApartmentList from '../../data/apartment.json'
 import Rating from '../../components/Rating'
 import Tag from '../../components/Tag';
 import InfoBloc from '../../components/InfoBloc'
+import Slider from '../../components/Slider'
 import React from "react";
 
 import '../../styles/logement.scss';
 
 
 const Logement = () => {
-    const { paramsId } = useParams()
-    const product = ApartmentList.find((product) => product.id === paramsId)
-
-    console.log(paramsId)
+    const {id} = useParams()
+    const product = ApartmentList.find((product) => product.id === id)
 
     return (
         <main className='logement'>
+            <Slider slides={product.pictures} />
             <div className="logement__name">
                 <div className="logement__name__titleZone">
-                    <h1>Cozy loft on the Canal Saint-Martin</h1>
-                    <p>Paris, Île-de-France</p>
+                    <h1>{product.title}</h1>
+                    <p>{product.location}</p>
                 </div>
                 <div className="logement__name__host">
                     <div className="logement__name__host__person_name">
-                        <p>Alexandre</p>
-                        <p>Dumas</p>
+                        <p>{product.host.name}</p>
+                        <img src={product.host.picture} alt="" />
                     </div>
                     <img src="" alt="" className="" />
                 </div>
             </div>
             <div className="logement__tag_rating">
                 <div className="logement__tag_rating__list">
-                    <Tag getTag="Maison"/>
-                    <Tag getTag="Garage"/>
+                    {product.tags.map((tag, index) => 
+                            <Tag key={index} getTag={tag}/>
+                        )}
                 </div>
-                <Rating rating="3"/>
+                <Rating rating={product.rating}/>
             </div>
             <div className='logement__infoZone'>
-                <InfoBloc className="logement__infoZone__info" title="Description" content="" />
-                <InfoBloc className="logement__infoZone__info"  title="Equipements" content=""/>
+                <InfoBloc className="logement__infoZone__info" title="Description" content={product.description} />
+                <InfoBloc className="logement__infoZone__info"  title="Equipements" content={product.equipments}/>
             </div>
         </main>
     )
